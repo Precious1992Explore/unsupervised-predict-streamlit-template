ff6c6d5fe49d5a16c9f9d2e3eae457e9db16a389
tshepo sibanyoni <trsiba@gmail.com>
2021-07-22 14:47:48 +0200

    modified the code

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -45,7 +45,7 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Welcome","About","EDA","Recommender System","Search for a movie","Solution Overview","Reviews","About Us"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -100,13 +100,210 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
+    if page_selection == "Search for a movie":
+        st.title("Search for Movies")
+        st.image(('resources/imgs/countdown.gif'), use_column_width=True)
+        st.markdown('Please Refer to the About Machine Learning Page to learn more about the techniques used to recommend movies. If you decide not to use the recommender systems you can use this page to filter movies based on the rating of the movie , the year in which the movie was released and the genre of the movies. After you change the filter you will be left with movies that are specific to that filter used. Then when you scroll down you will see the movie name and the link to a youtube trailer of that movie. When you click the link ,you will see a page on youtube for that specific movie and you can watch the trailer and see if you like it. This is an alternative method to you if you are not satisfied with the recommender engine . Enjoy! ', unsafe_allow_html=True)
+        # Movies
+        df = pd.read_csv('resources/data/movies.csv')
+        rating = pd.read_csv('resources/data/ratings.csv')
+        
+        def explode(df, lst_cols, fill_value='', preserve_index=False):
+            import numpy as np
+             # make sure `lst_cols` is list-alike
+            if (lst_cols is not None
+                    and len(lst_cols) > 0
+                    and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
+                lst_cols = [lst_cols]
+            # all columns except `lst_cols`
+            idx_cols = df.columns.difference(lst_cols)
+            # calculate lengths of lists
+            lens = df[lst_cols[0]].str.len()
+            # preserve original index values    
+            idx = np.repeat(df.index.values, lens)
+            # create "exploded" DF
+            res = (pd.DataFrame({
+                        col:np.repeat(df[col].values, lens)
+                        for col in idx_cols},
+                        index=idx)
+                    .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
+                            for col in lst_cols}))
+            # append those rows that have empty lists
+            if (lens == 0).any():
+                # at least one list in cells is empty
+                res = (res.append(df.loc[lens==0, idx_cols], sort=False)
+                            .fillna(fill_value))
+            # revert the original index order
+            res = res.sort_index()   
+            # reset index if requested
+            if not preserve_index:        
+                res = res.reset_index(drop=True)
+            return res 
+        movie_data = pd.merge(rating, df, on='movieId')
+        movie_data['year'] = movie_data.title.str.extract('(\(\d\d\d\d\))',expand=False)
+        #Removing the parentheses
+        movie_data['year'] = movie_data.year.str.extract('(\d\d\d\d)',expand=False)
+
+        movie_data.genres = movie_data.genres.str.split('|')
+        movie_rating = st.sidebar.number_input("Pick a rating ",0.5,5.0, step=0.5)
+
+        movie_data = explode(movie_data, ['genres'])
+        movie_title = movie_data['genres'].unique()
+        title = st.selectbox('Genre', movie_title)
+        movie_data['year'].dropna(inplace = True)
+        movie_data = movie_data.drop(['movieId','timestamp','userId'], axis = 1)
+        year_of_movie_release = movie_data['year'].sort_values(ascending=False).unique()
+        release_year = st.selectbox('Year', year_of_movie_release)
+
+        movie = movie_data[(movie_data.rating == movie_rating)&(movie_data.genres == title)&(movie_data.year == release_year)]
+        df = movie.drop_duplicates(subset = ["title"])
+        if len(df) !=0:
+            st.write(df)
+        if len(df) ==0:
+            st.write('We have no movies for that rating!')        
+        def youtube_link(title):
+    
+            """This function takes in the title of a movie and returns a Search query link to youtube
+    
+            INPUT: ('Avengers age of ultron')
+            -----------
+    
+            OUTPUT: https://www.youtube.com/results?search_query=The+little+Mermaid&page=1
+            ----------
+            """
+            title = title.replace(' ','+')
+            base = "https://www.youtube.com/results?search_query="
+            q = title
+            page = "&page=1"
+            URL = base + q + page
+            return URL            
+        if len(df) !=0:           
+            for _, row in df.iterrows():
+                st.write(row['title'])
+                st.write(youtube_link(title = row['title']))
+
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
+    if page_selection == "Welcome":
+        st.write('# Welcome To Our Movie Recommender')
+        st.image('resources/imgs/minions.jpg', use_column_width=True)
+        st.write('## ** * Where Movie Lovers Belong...* **')
+        #st.image('resources/imgs/Movie-Show-GIF-960.gif', use_column_width=True)
+        
 
+    if page_selection == 'About':
+        st.title("About")
+        st.markdown("Are you a movie lover? Are you tired of wasting your time watching tons of trailers and ending up not watching their movies? Are you tired of finishing your popcorns before you find the right movie? That has come to an end!!")
+        st.image("resources/imgs/welcome.gif",use_column_width=True)
+        st.markdown("Then we have got the right App for you.")
+        st.subheader("How The App Works")
+        st.markdown("The Movie Recommender App filters or predicts your preferences based on your favourite or watched movie selections. With just a few clicks, you will select three of your most favourite movies from thousands of movies on the app and you will get top 10 movies you are most likely to enjoy. You have an option to view some data visualizations including word clouds that show the most popular words that appear in movie titles and plots on the most popular genres. The app also contains a contact page, where users of the app can rate our app and give feedback and suggestions. Links to movie sites are also included, so the user has quick and easy to access the recommended movies.")
+        st.subheader("Data Description")
+        st.markdown("The dataset used for the movie recommender app consists of several million 5-star ratings obtained from users of the online MovieLens movie recommendation service. The data for the MovieLens dataset is maintained by the GroupLens research group in the Department of Computer Science and Engineering at the University of Minnesota. Additional movie content data was legally scraped from IMDB.")
+
+
+    if page_selection == 'EDA':
+        st.title('Exploratory Data Analysis')
+        st.image('resources/imgs/genres.png', use_column_width=True)
+        st.markdown(' This graph shows the number of movies per genre, from most to least')
+        st.markdown('--------')       
+
+        st.image('resources/imgs/Percentage of users per rating.png', use_column_width=True)
+        st.markdown('This graph shows the percentage of users per rating')
+        st.markdown('--------')
+
+        st.image('resources/imgs/total movies.png', use_column_width=True)
+        st.markdown('This graph shows the number of movies released per year')
+        st.markdown('--------')
+
+        st.image('resources/imgs/movie director.png', use_column_width=True)
+        st.markdown('This graphs shows the number of movies per director ')
+        st.markdown('--------')
+
+        st.image('resources/imgs/Popular actors.png', use_column_width=True)
+        st.markdown('This graph shows the top 20 of Popular actors')
+        st.markdown('--------')
+
+        st.image('resources/imgs/d of w movie title.png', use_column_width=True)
+        st.markdown('The visual above shows which words are commonly used when movies are titled')
+        st.markdown('--------')
+
+        st.image('resources/imgs/d of w movie title.png', use_column_width=True)
+        st.markdown('This visual shows popular directors')
+
+
+
+
+    if page_selection == "Solution Overview":
+        st.title("Solution Overview")
+        st.markdown("### CONTENT BASED FILTERING")
+        st.image('resources/imgs/content.png')
+        st.markdown('Uses item features to recommend other items similar to what the user likes, based on their previous actions or explicit feedback.')
+        
+
+        st.markdown('### COLLABORATIVE BASED FILTERING')
+        st.image('resources/imgs/collaborative.png')
+        st.markdown('builds a model from your past behavior (i.e. movies watched or selected by the you) as well as similar decisions made by other users.')
+        
+
+
+    if page_selection == 'Reviews':
+        st.title("Get in touch with us")
+        st.markdown('''<span style="color:blue"> **Help us improve this app by rating it. Tell us how to give you a better user experience.** </span>''', unsafe_allow_html=True)
+        @st.cache(allow_output_mutation=True)
+        def get_data():
+            return []
+        name = st.text_input("User name")
+        inputs = st.text_input("Let us improve your user experience!!!")
+        rate = st.slider("Rate us", 0, 5)
+        if st.button("Submit"):
+            get_data().append({"User name": name, "Suggestion": inputs,"rating":rate})
+        #st.markdown('''<span style="color:blue"> **What other users said:** </span>''', unsafe_allow_html=True)
+        #st.write(pd.DataFrame(get_data()))
+        st.markdown('''<span style="color:blue"> **For any questions contact us here:** </span>''', unsafe_allow_html=True)
+        st.markdown('chillfix@reviews.com')
+        #st.image(('resources/imgs/our contact2.PNG'), use_column_width=True)
+
+
+    if page_selection == 'About Us':
+        st.markdown("<h1 style='text-align: center; color: blue;'>About Us</h1>", unsafe_allow_html=True)
+        st.markdown("")
+        st.info("Palesa Kheswa")
+        st.markdown("* Github account:DineoPalesa")
+        st.markdown("* Kaggle account:Palesa Kheswa")
+        st.markdown("* email:palesadkheswa@gmail.com")
+        st.markdown("")
+
+        st.info("Precious Chauke")
+        st.markdown("* Github account:Precious1992Explore")
+        st.markdown("* Kaggle account:Precious Chauke")
+        st.markdown("* email:chaukeprecy@gmail.com")
+        st.markdown("")
+
+        st.info("Tshepo Sibanyoni")
+        st.markdown("* Github account:Trsiba")
+        st.markdown("* Kaggle account:Tshepo Sibanyoni")
+        st.markdown("* email:trsiba@gmail.com")
+        st.markdown("")
+
+        st.info("Zamangwane Dithebe")
+        st.markdown("* Github account:Zdithebe96")
+        st.markdown("* Kaggle account:Zamangwane Dithebe")
+        st.markdown("* email:zdithebe96@gmail.com")
+        st.markdown("")
+
+        st.info("Vuyelwa ")
+        #st.markdown("* Github account:")
+        st.markdown("* Kaggle account:VuyelwaF")
+        st.markdown("* email:vuyelwaf22@gmail.com")
+        st.markdown("")
+
+        st.info("Thato Kgoale")
+        #st.markdown("* Github account:")
+        st.markdown("* Kaggle account:ThatoK")
+        st.markdown("* email:kgoalethato19@gmail.com")
+        st.markdown("")
 
 if __name__ == '__main__':
     main()
